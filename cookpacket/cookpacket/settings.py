a0e1691a008d1d616f68a031c9c2b2edf9951270
ronythankachan <rony.mail2m@gmail.com>
2019-01-10 03:14:54 +0530

    navbar almost working

--- cookpacket/cookpacket/settings.py
+++ cookpacket/cookpacket/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'cookpacket.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS':[os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
